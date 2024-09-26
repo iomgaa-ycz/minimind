514a7ac0fd83106add5213650cf27ee1a0421a2f
Yu Chengzhang <iomgaaycz@gmail.com>
2024-09-26 16:17:19 +0800

    第一版的Accelerator代码

---
+++ Accelerator/1-pretrain.py
@@ -0,0 +1,162 @@
+import os
+import platform
+import argparse
+import time
+import math
+import warnings
+import torch
+import torch.distributed as dist
+from torch import optim
+from torch.nn.parallel import DistributedDataParallel
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader, DistributedSampler
+from contextlib import nullcontext
+from model.model import Transformer
+from model.LMConfig import LMConfig
+from model.dataset import PretrainDataset
+from accelerate import Accelerator
+
+warnings.filterwarnings('ignore')
+
+
+def Logger(content):
+    if accelerator.is_local_main_process:
+        print(content)
+
+
+def get_lr(it, all):
+    warmup_iters = args.warmup_iters
+    lr_decay_iters = all
+    min_lr = args.learning_rate / 10
+
+    if it < warmup_iters:
+        return args.learning_rate * it / warmup_iters
+    if it > lr_decay_iters:
+        return min_lr
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (args.learning_rate - min_lr)
+
+
+def train_epoch(epoch, wandb):
+    start_time = time.time()
+    for step, (X, Y) in enumerate(train_loader):
+        lr = get_lr(epoch * iter_per_epoch + step, args.epochs * iter_per_epoch)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+        with accelerator.accumulate(model):
+            out = model(X, Y)
+            loss = out.last_loss
+            accelerator.backward(loss)
+
+            if accelerator.sync_gradients:
+                accelerator.clip_grad_norm_(model.parameters(), args.grad_clip)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if step % args.log_interval == 0:
+            spend_time = time.time() - start_time
+            Logger(
+                'Epoch:[{}/{}]({}/{}) loss:{:.3f} lr:{:.7f} epoch_Time:{}min:'.format(
+                    epoch,
+                    args.epochs,
+                    step,
+                    iter_per_epoch,
+                    loss.item(),
+                    lr,
+                    spend_time / (step + 1) * iter_per_epoch // 60 - spend_time // 60))
+
+            if wandb is not None and accelerator.is_local_main_process:
+                wandb.log({"loss": loss.item(),
+                           "lr": lr,
+                           "epoch_Time": spend_time / (step + 1) * iter_per_epoch // 60 - spend_time // 60})
+
+        if (step + 1) % args.save_interval == 0 and accelerator.is_local_main_process:
+            accelerator.wait_for_everyone()
+            model.eval()
+            moe_path = '_moe' if lm_config.use_moe else ''
+            ckp = f'{args.save_dir}/pretrain_{lm_config.dim}{moe_path}.pth'
+
+            unwrapped_model = accelerator.unwrap_model(model)
+            accelerator.save(unwrapped_model.state_dict(), ckp)
+            model.train()
+
+
+def init_model():
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    model = Transformer(lm_config)
+    moe_path = '_moe' if lm_config.use_moe else ''
+
+    Logger(f'LLM总参数量：{count_parameters(model) / 1e6:.3f} 百万')
+    return model
+
+
+# torchrun --nproc_per_node 2 1-pretrain.py
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MiniMind Pretraining")
+    parser.add_argument("--out_dir", type=str, default="out", help="Output directory")
+    parser.add_argument("--epochs", type=int, default=20, help="Number of epochs")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
+    parser.add_argument("--learning_rate", type=float, default=2e-4, help="Learning rate")
+    parser.add_argument("--dtype", type=str, default="bfloat16", help="Data type")
+    parser.add_argument("--use_wandb", action="store_true", help="Use Weights & Biases")
+    parser.add_argument("--wandb_project", type=str, default="MiniMind-Pretrain", help="Weights & Biases project name")
+    parser.add_argument("--num_workers", type=int, default=8, help="Number of workers for data loading")
+    parser.add_argument("--data_path", type=str, default="./dataset/pretrain_data.bin", help="Path to training data")
+    parser.add_argument("--accumulation_steps", type=int, default=8, help="Gradient accumulation steps")
+    parser.add_argument("--grad_clip", type=float, default=1.0, help="Gradient clipping threshold")
+    parser.add_argument("--warmup_iters", type=int, default=0, help="Number of warmup iterations")
+    parser.add_argument("--log_interval", type=int, default=100, help="Logging interval")
+    parser.add_argument("--save_interval", type=int, default=1000, help="Model saving interval")
+
+    args = parser.parse_args()
+
+    lm_config = LMConfig()
+    max_seq_len = lm_config.max_seq_len
+    args.save_dir = os.path.join(args.out_dir)
+    os.makedirs(args.save_dir, exist_ok=True)
+    os.makedirs(args.out_dir, exist_ok=True)
+    tokens_per_iter = args.batch_size * max_seq_len
+    torch.manual_seed(1337)
+
+    args.wandb_run_name = f"MiniMind-Pretrain-Epoch-{args.epochs}-BatchSize-{args.batch_size}-LearningRate-{args.learning_rate}"
+
+    accelerator = Accelerator(gradient_accumulation_steps=args.accumulation_steps)
+
+    if args.use_wandb and accelerator.is_local_main_process:
+        import wandb
+        os.environ['WANDB_MODE'] = 'offline'
+        wandb.init(project=args.wandb_project, name=args.wandb_run_name)
+    else:
+        wandb = None
+
+    data_path_list = [args.data_path]
+    train_ds = PretrainDataset(data_path_list, max_length=max_seq_len, memmap=True)
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=args.batch_size,
+        pin_memory=True,
+        drop_last=False,
+        shuffle=False,
+        num_workers=args.num_workers,
+    )
+
+    model = init_model()
+
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    model, optimizer, train_loader = accelerator.prepare(model, optimizer, train_loader)
+
+    if False and platform.system() != 'Windows' and float(torch.__version__.split('.')[0]) >= 2:
+        Logger("compiling the model... (takes a ~minute)")
+        unoptimized_model = model
+        model = torch.compile(model)
+
+    iter_per_epoch = len(train_loader)
+    for epoch in range(args.epochs):
+        train_epoch(epoch, wandb)
